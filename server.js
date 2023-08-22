1cb835fd7f8cf9c8c47db4ef1407ba2066cef16b
Davi de Almeida Nascimento <118446616+dexterpol@users.noreply.github.com>
2023-08-22 11:42:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,29 +3,53 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
+  if (req.method === 'GET') {
+    let filePath = path.join(__dirname, '../cliente-servidor', req.url);
     
-    const filePath = path.join(__dirname, 'index.html');
-    fs.readFile(filePath, 'utf-8', (err, content) => {
+    if (req.url === '/') {
+      filePath = path.join(__dirname, '../cliente-servidor/login.html');
+    }
+
+    const fileExtension = path.extname(filePath);
+    let contentType = 'text/html';
+
+    if (fileExtension === '.css') {
+      contentType = 'text/css';
+    } else if (fileExtension === '.js') {
+      contentType = 'application/javascript';
+    }
+
+    fs.readFile(filePath, (err, content) => {
       if (err) {
         res.writeHead(500);
         res.end('Erro interno do servidor');
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { 'Content-Type': contentType });
         res.end(content);
       }
     });
   } else if (req.method === 'POST' && req.url === '/cadastro') {
-    
     let data = '';
     req.on('data', chunk => {
       data += chunk;
     });
-    req.on('end', () => {
-      const parsedData = JSON.parse(data);
-      
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'Cadastro realizado com sucesso!' }));
+    req.on('end', async () => {
+      try {
+        const parsedData = JSON.parse(data);
+        
+        
+        const nome = parsedData.nome;
+        const email = parsedData.email;
+        const senha = parsedData.senha;
+
+        
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: `Cadastro realizado para ${nome} com sucesso!` }));
+      } catch (error) {
+        console.error('Erro ao analisar JSON:', error);
+        res.writeHead(400);
+        res.end('Erro ao processar dados de cadastro');
+      }
     });
   } else {
     res.writeHead(404);
